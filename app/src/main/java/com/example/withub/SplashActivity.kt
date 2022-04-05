1f1935dfdd0c857d259bbdd2306f563ec0a8d55b
SJMoons <ferrari_919@naver.com>
2022-04-05 21:02:44 +0900

    UPDATE ui

--- app/src/main/java/com/example/withub/SplashActivity.kt
+++ app/src/main/java/com/example/withub/SplashActivity.kt
@@ -2,10 +2,13 @@ package com.example.withub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash_activity)
+
+        Log.d("message","abddsf")
     }
 }

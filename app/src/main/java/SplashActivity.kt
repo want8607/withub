b7f01c329b744f65786796482b6eb39c0a474efd
SJMoons <ferrari_919@naver.com>
2022-04-08 18:30:33 +0900

    UPDATE setting ui

--- app/src/main/java/SplashActivity.kt
+++ app/src/main/java/SplashActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.emailcertify_fragment)
+        setContentView(R.layout.findpw_activity)
     }
 }

b908301e0df31c6280e3b2262f68e68e486adf8e
SJMoons <ferrari_919@naver.com>
2022-04-25 19:30:32 +0900

    UPDATE start

--- app/src/main/java/SplashActivity.kt
+++ app/src/main/java/SplashActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.findpw_activity)
+        setContentView(R.layout.login_activity)
     }
 }
